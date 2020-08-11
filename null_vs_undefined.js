ab8ab706e8a3b5c1d5282a4942d380a03820563b
eisenham <anmfahimfaisal@gmail.com>
2020-08-11 17:43:40 +0600

    null vs undefined

---
+++ null_vs_undefined.js
@@ -0,0 +1,22 @@
+//let Bird;
+
+// function add(num1, num2){
+//     console.log(num1+num2);
+//     return 
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+//const result = add (13);
+// const guy = {name: "Samuel", phone: 4563299};
+// console.log(guy.number);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
+
+
